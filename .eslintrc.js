f61990809d8ecefc2eca43c74158fc31c9754eef
Kevin Ross <kevin.ross@alienfast.com>
2022-10-04 16:16:34 -0500

    fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,13 @@ module.exports = {
     // this gets really messy in tsx and graphql when types are forced to any e.g. policies
     '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off'
-  }
+  },
+    parserOptions: {
+    project: [
+      './tsconfig*.json', // for root project ref and config files etc *outside* of src directories
+      './packages/*/tsconfig.json', // all code *in* src directories
+    ],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
 };
