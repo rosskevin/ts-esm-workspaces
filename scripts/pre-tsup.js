e46da9c4d061fc554b06373728c1dff013048805
Kevin Ross <kevin.ross@alienfast.com>
2022-10-05 13:11:21 -0500

    remove invalid metadata for tsc -b

--- scripts/pre-tsup.js
+++ scripts/pre-tsup.js
@@ -6,3 +6,10 @@ rimraf('./packages/*/dist/**/*.{js,js.map}', (error) => {
     throw error
   }
 })
+
+// remove incremental build metadata - as it is no longer valid.
+rimraf('./**/tsconfig.tsbuildinfo', (error) => {
+  if (error) {
+    throw error
+  }
+})
