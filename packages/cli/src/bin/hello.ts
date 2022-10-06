25274557778b109685dd2fd867f1d8713e44b051
Kevin Ross <kevin.ross@alienfast.com>
2022-10-06 14:15:49 -0500

    Proof of working bin with esm only and referencing an esm-only version of our loggers

--- packages/cli/src/bin/hello.ts
+++ packages/cli/src/bin/hello.ts
@@ -3,9 +3,15 @@
  * Simplest cli tool we can create - exercise esm as well
  */
 
+import { Logger } from '@alienfast/logger'
+import { initializeNodeLogger } from '@alienfast/logger-node'
 import { inc } from '@rosskevin/ts-esm-workspaces-shared'
 import { execaSync } from 'execa'
 import { merge } from 'lodash-es'
+
+initializeNodeLogger()
+
+const log = Logger.get('hello')
 
 const count = 100
 const message = `world ${inc(count)}`
@@ -15,3 +21,5 @@ console.log(stdout)
 
 const foo = merge({}, { hello: 'world' })
 console.log(foo)
+
+log.info('Some logged message.')
--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
   clean: true,
-  // dts: true,
+  // dts: true, // bug with project references https://github.com/egoist/tsup/issues/647#issuecomment-1268777707
   format: ['esm'], // 'cjs'],
   minify: true,
   // async onSuccess() {
